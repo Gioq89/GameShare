37549dc8a9da96b546459e8949387bfaf0c21ecb
giomendes.quezada <gioq89@gmail.com>
2023-07-12 19:50:42 -0700

    added seeds and auth

---
+++ seeds/seed.js
@@ -0,0 +1,39 @@
+const sequelize = require("../config/connection");
+const { User, Game, UserGames } = require("../models");
+
+const userData = require("./userData.json");
+const gameData = require("./gameData.json");
+const userGamesData = require("./userGamesData.json");
+
+const seedDatabase = async () => {
+  try {
+    await sequelize.sync({ force: true });
+
+    // Seed users
+    await User.bulkCreate(userData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    // Seed games
+    await Game.bulkCreate(gameData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    // Seed usergames
+    await UserGames.bulkCreate(userGamesData, {
+      individualHooks: true,
+      returning: true,
+    });
+
+    console.log("Database seeded successfully!");
+
+    process.exit(0);
+  } catch (err) {
+    console.log("Error seeding database", error);
+    process.exit(1);
+  }
+};
+
+seedDatabase();
