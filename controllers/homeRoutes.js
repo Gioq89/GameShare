76532fd9ef0c23a13fdd392043bb30752e6eeaea
lpakingan <lpak98@gmail.com>
2023-07-13 19:29:24 -0700

    add route for showing games on dashboard

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,22 @@ const router = require('express').Router();
 const { Game, User } = require('../models');
 const withAuth = require('../utils/auth');
 // --------------------------------------------------------------
-// TO DO: Add a dashboard that shows games?
+// GET all games and show them on the homepage
+router.get("/", async (req, res) => {
+  try {
+    const gameData = await Game.findAll();
+
+    const games = gameData.map((game) => game.get({ plain: true }));
+
+    // render the homepage and pass that the user is logged in
+    res.render("dashboard", {
+      games,
+      // logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 // redirect to the login page if the user is not logged in
 router.get('/login', (req, res) => {
