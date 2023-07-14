81ec2b8eaa91b3fdf90f17bc3a5d7ec452ef7eee
lpakingan <lpak98@gmail.com>
2023-07-14 12:11:50 -0700

    add route for gameInfo handlebars rendering

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,9 +1,10 @@
 const router = require('express').Router();
 const { Game, User } = require('../models');
+const axios = require('axios');
 const withAuth = require('../utils/auth');
 // --------------------------------------------------------------
 // GET all games and show them on the homepage
-router.get("/", async (req, res) => {
+router.get("/dashboard", async (req, res) => {
   try {
     const gameData = await Game.findAll();
 
@@ -12,6 +13,35 @@ router.get("/", async (req, res) => {
     // render the homepage and pass that the user is logged in
     res.render("dashboard", {
       games,
+      // logged_in: req.session.logged_in,
+    });
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET one game by clicking on it to get full info 
+router.get("/dashboard/games/:id", async (req, res) => {
+  try {
+    const gameData = await Game.findByPk(req.params.id, {
+      where: {
+        id: req.params.id
+      }
+    });
+    const game = gameData.get({ plain: true });
+
+    // call in the API to get full info on a game (since this information is unable to be retrieved from the get all route for the local database)
+    const response = await axios.get(
+      `https://www.freetogame.com/api/game?id=${req.params.id}`,
+    );
+    const info = response.data;
+    const screenshots = info.screenshots;
+
+    // render the homepage and pass that the user is logged in
+    res.render("gameInfo", {
+      game,
+      info,
+      screenshots,
       // logged_in: req.session.logged_in,
     });
   } catch (error) {
