fb74669c5677eaa9223243e79e1989aface348ea
giomendes.quezada <gioq89@gmail.com>
2023-07-17 18:30:47 -0700

    added login route and login.js to handle button submisson

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const gameRoutes = require('./gameRoutes');
 const userRoutes = require('./userRoutes');
+const loginRoutes = require('./loginRoutes');
 
 router.use('/games', gameRoutes);
 router.use('/users', userRoutes);
+router.use('/login', loginRoutes);
 
 module.exports = router;
