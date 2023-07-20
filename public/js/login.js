bc6146476bb9f4d95a89a85d7caa1e09cce5cf08
Riku Choy <rikuchoy@gmail.com>
2023-07-19 20:05:53 -0700

    added additional styling to login page, game info, and connect

--- public/js/login.js
+++ public/js/login.js
@@ -22,4 +22,30 @@ const loginFormHandler = async (event) => {
   }
 };
 
+// login.js
+document.addEventListener("DOMContentLoaded", function () {
+  const title = document.querySelector(".game-font");
+
+  // Set initial text-stroke properties
+  title.style.webkitTextStrokeWidth = "2px";
+  title.style.webkitTextStrokeColor = "black";
+  title.style.opacity = 0;
+
+  // Define the animation for drawing the text
+  const drawAnimation = anime({
+    targets: title,
+    opacity: 1,
+    webkitTextStrokeWidth: 0,
+    easing: "easeInOutQuad",
+    duration: 2000,
+    autoplay: false, // We won't play the animation right away
+  });
+
+  // Play the drawing animation
+  drawAnimation.play();
+});
+
+
+
+
 loginBtn.addEventListener('click', loginFormHandler);
